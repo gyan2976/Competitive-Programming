a42ae4ce32880c6d743ad76e3c968172c877e4bd
Gyan Ranjan Kumar <103508999+gyan2976@users.noreply.github.com>
2025-01-16 18:31:16 +0530

    Add files via upload

---
+++ Searching & Sorting/Bubble Sort.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <stdio.h>
+#include <climits>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+void bubbleSort(int *arr, int n)
+{
+    for(int i = 0; i < n-1; i++)
+    {
+        for(int j = i+1; j < n; j++)
+        {
+            if(arr[i] > arr[j])
+            {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+
+        int arr[n];
+        for(int i = 0; i < n; i++)
+        {
+            cin >> arr[i];
+        }
+
+        bubbleSort(arr, n);
+
+        for(int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+
+    return 0;
+}
