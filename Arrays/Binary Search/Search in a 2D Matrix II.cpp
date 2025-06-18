8d9b4097677903e390b20c150cde65f11f12e960
Gyan Ranjan Kumar <103508999+gyan2976@users.noreply.github.com>
2025-06-18 18:33:54 +0530

    Add files via upload

---
+++ Arrays/Binary Search/Search in a 2D Matrix II.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+#include <limits.h>
+#include <stdio.h>
+#include <climits>
+#include <stdlib.h>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cctype> 
+#include <vector>
+#include <stack>
+#include <queue>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+// Brute Force - O(n*m)
+bool searchMatrix(vector<vector<int>>& matrix, int target)
+{
+    int n = matrix.size(), m = matrix[0].size();
+
+    //traverse the matrix:
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == target)
+                return true;
+        }
+    }
+    return false;
+}
+
+// Better Solution - O(n*log(m))
+bool binarySearch(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    int low = 0, high = n - 1;
+
+    // Perform the steps:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target) return true;
+        else if (target > nums[mid]) low = mid + 1;
+        else high = mid - 1;
+    }
+    return false;
+}
+
+bool searchMatrix(vector<vector<int>>& matrix, int target)
+{
+    int n = matrix.size();
+    for (int i = 0; i < n; i++) {
+        bool flag =  binarySearch(matrix[i], target);
+        if (flag) return true;
+    }
+    return false;
+}
+
+// Optimal Solution - O(n+m)
+bool searchMatrix(vector<vector<int>>& matrix, int target)
+{
+    int n = matrix.size();
+    int m = matrix[0].size();
+    int row = 0, col = m - 1;
+
+    //traverse the matrix from (0, m-1):
+    while (row < n && col >= 0) {
+        if (matrix[row][col] == target) return true;
+        else if (matrix[row][col] < target) row++;
+        else col--;
+    }
+    return false;
+}
+
+int main()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        vector<vector<int>> matrix = {{1, 4, 7, 11, 15}, 
+                                      {2, 5, 8, 12, 19},
+                                      {3, 6, 9, 16, 22}, 
+                                      {10, 13, 14, 17, 24},
+                                      {18, 21, 23, 26, 30}};
+
+        searchMatrix(matrix, 8) == true ? cout << "true\n" : cout << "false\n";
+    }
+
+    return 0;
+}
